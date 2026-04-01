4e48f5fbf9644b9364cad7861c3e1b60ce52c22a
quanglee16 <ledangquang16022005@gmail.com>
2026-04-01 07:41:15 +0700

    Modify format and comment in Problem 1

--- Assignment5/Problem1.c
+++ Assignment5/Problem1.c
@@ -2,76 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Định nghĩa cấu trúc bản ghi
+//TODO Define struct record
 typedef struct {
     char firstname[50];
     char lastname[50];
     int age;
 } Student;
 
-// --- 1. Hàm callback so sánh theo First Name ---
-int compare_firstname(const void *pa, const void *pb) {
-    const Student *s1 = (const Student *)pa;
-    const Student *s2 = (const Student *)pb;
+//TODO Function callback to compare about firstname
+int compare_firstname(const void* pa, const void* pb) {
+    const Student* s1 = (const Student*) pa;
+    const Student* s2 = (const Student*) pb;
     return strcmp(s1->firstname, s2->firstname);
 }
 
-// --- 2. Hàm callback so sánh theo Last Name ---
-int compare_lastname(const void *pa, const void *pb) {
-    const Student *s1 = (const Student *)pa;
-    const Student *s2 = (const Student *)pb;
+//TODO Function callback to compare about lastname
+int compare_lastname(const void* pa, const void* pb) {
+    const Student* s1 = (const Student*) pa;
+    const Student* s2 = (const Student*) pb;
     return strcmp(s1->lastname, s2->lastname);
 }
 
-// --- 3. Hàm isolder() ---
-void isolder(Student *s) {
+//TODO Function to print info of record has age > 20
+void isolder(Student* s) {
     if (s->age > 20) {
-        printf("%-10s %-10s | %d tuoi\n", s->firstname, s->lastname, s->age);
+        printf("%s %s | %d tuoi\n", s->firstname, s->lastname, s->age);
     }
 }
 
-// --- 4. Hàm apply() ---
-// Duyệt mảng và gọi hàm callback fp cho mỗi phần tử
-void apply(Student *arr, int num, void (*fp)(Student *)) {
+//TODO Iterative array records and call function on each element
+void apply(Student* arr, int num, void (*fp)(Student*)) {
     for (int i = 0; i < num; i++) {
         fp(&arr[i]);
     }
 }
 
-// Hàm hỗ trợ in mảng
+//TODO Function that print array
 void print_students(Student *arr, int n) {
     for (int i = 0; i < n; i++) {
-        printf("%-10s %-10s | %d\n", arr[i].firstname, arr[i].lastname, arr[i].age);
+        printf("%s %s | %d\n", arr[i].firstname, arr[i].lastname, arr[i].age);
     }
 }
 
 int main() {
-    // Khởi tạo dữ liệu mẫu
-    Student celebs[] = {
-        {"Son", "Tung", 29},
-        {"Chi", "Pu", 30},
-        {"Den", "Vau", 34},
-        {"Amee", "Tran", 23},
-        {"Mono", "Nguyen", 20}
+    Student list_student[] = {
+        {"Quang", "Le", 22},
+        {"Nice", "Try", 30},
+        {"ABC", "XYZ", 34},
+        {"Dang", "Le", 23},
+        {"Quang", "Le Dang", 20}
     };
-    int n = sizeof(celebs) / sizeof(celebs[0]);
+    int n = sizeof(list_student) / sizeof(list_student[0]);
 
-    printf("=== Danh sach goc ===\n");
-    print_students(celebs, n);
+    printf("List\n");
+    print_students(list_student, n);
 
-    // Sắp xếp theo Tên
-    printf("\n=== Sap xep theo First Name ===\n");
-    qsort(celebs, n, sizeof(Student), compare_firstname);
-    print_students(celebs, n);
+    //* Sort about firsname
+    printf("\nSort about firsname\n");
+    qsort(list_student, n, sizeof(Student), compare_firstname);
+    print_students(list_student, n);
 
-    // Sắp xếp theo Họ
-    printf("\n=== Sap xep theo Last Name ===\n");
-    qsort(celebs, n, sizeof(Student), compare_lastname);
-    print_students(celebs, n);
+    //* Sort about lastname
+    printf("\nSort about lastname\n");
+    qsort(list_student, n, sizeof(Student), compare_lastname);
+    print_students(list_student, n);
 
-    // Sử dụng hàm apply và isolder
-    printf("\n=== Danh sach nhung nguoi > 20 tuoi ===\n");
-    apply(celebs, n, isolder);
+    printf("\nList Students that have age large 20\n");
+    apply(list_student, n, isolder);
 
     return 0;
 }
